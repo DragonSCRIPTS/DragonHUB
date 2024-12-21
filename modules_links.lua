dca995727b60e684a9aedcd599aa875de9e8ab4f
DragonSCRIPTS <cleyltondragonscp@gmail.com>
2024-12-21 12:07:52 -0300

    Update modules_links.lua

--- modules_links.lua
+++ modules_links.lua
@@ -1,25 +1,61 @@
--- Módulos de Link com nome
-local modules = {
-    -- AntiKick Module
-    {name = "AntiKick Module", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/AntiKick%20Module.lua"},
-    
-    -- FlagPlayer Module
-    {name = "FlagPlayer Module", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/FlagPlayer%20Module.lua"},
-    
-    -- SeaWorld Module
-    {name = "SeaWorld Module", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/SeaWorld%20Module.lua"},
-    
-    -- Anti AFK Module
-    {name = "Anti AFK Module", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/Anti%20AFK%20Module.lua"}
+-- Funções do menu Configurações
+local configuracoes = {
+    {name = "Módulo AntiKick", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/AntiKick%20Module.lua"},
+    {name = "Módulo FlagPlayer", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/FlagPlayer%20Module.lua"},
+    {name = "Módulo SeaWorld", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/SeaWorld%20Module.lua"},
+    {name = "Módulo Anti AFK", url = "https://raw.githubusercontent.com/DragonSCRIPTS/DragonHUB/refs/heads/main/Anti%20AFK%20Module.lua"}
 }
 
--- Função para carregar os módulos
-local function loadModules()
-    for _, module in ipairs(modules) do
-        print("Carregando módulo: " .. module.name)  -- Exibe o nome do módulo
-        loadstring(game:HttpGet(module.url))()  -- Carrega e executa o script do módulo
-    end
-end
+-- Funções do menu Principal
+local principal = {
+    {name = "Módulo Principal 1", url = "https://link_ficticio.com/Main%20Module%201.lua"},
+    {name = "Módulo Principal 2", url = "https://link_ficticio.com/Main%20Module%202.lua"}
+}
 
--- Chama a função para carregar os módulos
-loadModules()
+-- Funções do menu Estatísticas
+local estatisticas = {
+    {name = "Módulo Estatísticas 1", url = "https://link_ficticio.com/Stats%20Module%201.lua"},
+    {name = "Módulo Estatísticas 2", url = "https://link_ficticio.com/Stats%20Module%202.lua"}
+}
+
+-- Funções do menu Jogador
+local jogador = {
+    {name = "Módulo Jogador 1", url = "https://link_ficticio.com/Player%20Module%201.lua"},
+    {name = "Módulo Jogador 2", url = "https://link_ficticio.com/Player%20Module%202.lua"}
+}
+
+-- Funções do menu Teleporte
+local teleporte = {
+    {name = "Módulo Teleporte 1", url = "https://link_ficticio.com/Teleport%20Module%201.lua"},
+    {name = "Módulo Teleporte 2", url = "https://link_ficticio.com/Teleport%20Module%202.lua"}
+}
+
+-- Funções do menu Frutas
+local frutas = {
+    {name = "Módulo Frutas 1", url = "https://link_ficticio.com/Fruit%20Module%201.lua"},
+    {name = "Módulo Frutas 2", url = "https://link_ficticio.com/Fruit%20Module%202.lua"}
+}
+
+-- Funções do menu Raid
+local raid = {
+    {name = "Módulo Raid 1", url = "https://link_ficticio.com/Raid%20Module%201.lua"},
+    {name = "Módulo Raid 2", url = "https://link_ficticio.com/Raid%20Module%202.lua"}
+}
+
+-- Funções do menu Corrida
+local corrida = {
+    {name = "Módulo Corrida 1", url = "https://link_ficticio.com/Race%20Module%201.lua"},
+    {name = "Módulo Corrida 2", url = "https://link_ficticio.com/Race%20Module%202.lua"}
+}
+
+-- Funções do menu Loja
+local loja = {
+    {name = "Módulo Loja 1", url = "https://link_ficticio.com/Shop%20Module%201.lua"},
+    {name = "Módulo Loja 2", url = "https://link_ficticio.com/Shop%20Module%202.lua"}
+}
+
+-- Funções do menu Diversos
+local diversos = {
+    {name = "Módulo Diversos 1", url = "https://link_ficticio.com/Misc%20Module%201.lua"},
+    {name = "Módulo Diversos 2", url = "https://link_ficticio.com/Misc%20Module%202.lua"}
+}
